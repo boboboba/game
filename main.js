62bc49656021dfa59ac807a717f9bec4ad7c0275
boboboba <95755392+boboboba@users.noreply.github.com>
2021-12-20 21:53:49 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
+'use strict'
 
 let		canvas = document.createElement('canvas'),
-		ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d'),
+
+
+		w		= canvas.width  = window.innerWidth,
+		h 		= canvas.height = window.innerHeight;
+		document.querySelector('body').appendChild(canvas);
 
 // game set
-const 	width 		= canvas.width  = window.innerWidth,
-		height 		= canvas.height = window.innerHeight,
-		half_width 	= Math.floor(width/2),
-		half_height = Math.floor(height/2),
-		tile 		= 100,
+const	tile 		= 100,
 		fov 		= Math.PI/3,
-		num_rays	= 60,
-		max_dist	= 2000,
+		num_rays	= 120,
+		max_dist	= 1000,
 		d_angle		= fov/num_rays,
 		surface_dist= num_rays / (2*Math.tan(fov/2)),
-		scale		= Math.floor(width/num_rays),
+		
 
 
 //map
 		text_map = [
-		'@@@@@@@@',
-		'@......@',
-		'@......@',
-		'@..@...@',
-		'@......@',
-		'@..@...@',
-		'@......@',
-		'@@@@@@@@',		
-		]
+		'@@@@@@@@@@@',
+		'@.........@',
+		'@...@..@@.@',
+		'@.........@',
+		'@.@.......@',
+		'@.@....@..@',
+		'@.........@',
+		'@@@@@@@@@@@',		
+		],
 
 
 		
@@ -37,19 +39,25 @@ const 	width 		= canvas.width  = window.innerWidth,
 		red 	= 'rgb(255, 0, 0)',
 		yellow 	= 'rgb(255, 255, 0)',
 		green	= 'rgb(0, 128, 0)',
-		blue	= 'rgb(0, 0, 255)'
+		blue	= 'rgb(0, 0, 255)',
 
 		bgcolor = black;
 
 //player set
 let		player_set 	= {
-			x		: half_width,
-			y 		: half_height,
+			x		: 150,
+			y 		: 250,
 			angle	: 0,
-			speed	: 5
+			speed	: 3
 		},
+		scale		= (w/num_rays)+2;
+		
+window.onresize = function(){
+w		= canvas.width = innerWidth,
+h		= canvas.height = innerHeight;   
+scale	= (w/num_rays)+2;     
+};
 
-		cos,sin;
 
 
 let keys = [false,false,false,false,false,false,false,false];
@@ -62,13 +70,13 @@ class Player{
 	}
 
 	movement(){		
-		cos = Math.cos(player_set.angle);
-		sin = Math.sin(player_set.angle);		
+		let cos = Math.cos(player_set.angle),
+			sin = Math.sin(player_set.angle);		
 		if (keys[4]) {
-			player_set.angle -= 0.05;
+			player_set.angle -= 0.02;
 		}  
 		if (keys[5]) {    
-			player_set.angle += 0.05;
+			player_set.angle += 0.02;
 		}
 		if (keys[0]) {
 			this.x += player_set.speed*cos;
@@ -100,42 +108,42 @@ class Player{
 
 	
 
-}
+};
 
-player = new Player();
+let player = new Player();
 
-
-document.querySelector('body').appendChild(canvas);
 
 let map = new Set();
-
+let b = new Array(0,0);
 text_map.forEach((row, y) => {
 	Array.from(row).forEach((cell, x) => {
+		
 		if (cell == '@') {
-			map.add([x*tile,y*tile])
+			map.add(String(x*tile)+String(y*tile));
+			
 	}
 	});
 });
 
-console.log(scale);
+console.log(typeof(b));
 
 function rayCast(Px,Py,angle){
-	cur_angle = angle - fov/2;
+	let cur_angle = angle - fov/2;
 	let xo = Px,
 		yo = Py,
 		sin,cos,x,y,d;
-	for (let i = 0; i<num_rays;++i){
+	for (let i = 0; i<num_rays;i+=1){
 		sin = Math.sin(cur_angle);
 		cos = Math.cos(cur_angle);
 		for (let dist=0; dist < max_dist; dist+=2){
 			x = xo + dist*cos;
 			y = yo + dist*sin;
-			if (x > width || x < 0 || y > height || y < 0){
+			if (map.has(String(x-x%tile)+String(y-y%tile))){
 				dist *= Math.cos(player_set.angle - cur_angle);
 				ctx.beginPath();
 				let c = 255/(1+dist*dist*0.0001)+16;
-				ctx.fillStyle = 'rgb('+c+','+c+','+c+')';
-				ctx.fillRect(i*width/num_rays, half_height - 80000/dist/2, scale+1,80000/dist);
+				ctx.fillStyle = 'rgb('+c+','+(c+64)+','+c+')';
+				ctx.fillRect(i*w/num_rays, h/2 - 80000/dist/2, scale,80000/dist);
 				ctx.closePath();
 				ctx.fill();
 
@@ -150,7 +158,7 @@ function rayCast(Px,Py,angle){
 function redrawBackground(){
 	ctx.beginPath();
 	ctx.fillStyle = bgcolor;
-	ctx.fillRect(0,0,width,height);
+	ctx.fillRect(0,0,w,h);
 	ctx.closePath();
 	
 	// map.forEach((row,i) => {
@@ -223,10 +231,19 @@ document.addEventListener("keyup", (e) => {
 	if (e.keyCode === 68) {
 		keys[3] = false;
 	}
-	if (e.keyCode === 37) {
-		keys[4] = false;
-	}
-	if (e.keyCode === 39) {
-		keys[5] = false;
-	}
+// 	if (e.keyCode === 37) {
+// 		keys[4] = false;
+// 	}
+// 	if (e.keyCode === 39) {
+// 		keys[5] = false;
+// 	}
+ });
+
+document.addEventListener("mousemove", function (event) {
+  player_set.angle += event.movementX*fov/w;
 });
+
+canvas.addEventListener("click", () => {
+  canvas.requestPointerLock();
+});
+
